d7be25d672f57f50e6279d1dfbe7405206c6bdb0
Dima <dimaszver@live.com>
2023-07-11 18:52:19 +0300

    Решили 3 задачу

--- les1.2.py
+++ les1.2.py
@@ -1,1 +1,8 @@
 ## 3 Задача
+year = int(input ('Введите год - '))
+if year%4 == 0 or year%400 == 0 and year%100 != 0:
+    print (f'{year} Год високосный')
+else:
+     print (f'{year} Год невисокосный')
+
+
