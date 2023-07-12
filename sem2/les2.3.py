44e3c9f3da9d9f5c0a221dbad79b2b96c0b2c691
Dima <dimaszver@live.com>
2023-07-12 18:01:07 +0300

    подредактировали 3 задачу

--- sem2/les2.3.py
+++ sem2/les2.3.py
@@ -1,6 +1,9 @@
 ## Задача на нахождение сколько дней длилась самая длинная оттепель
 date = [int(i) for i in input('введите числа через пробел: ').split()]
 n = len(date)
+if n > 10:
+    print('Превышено значение')
+    exit()
 max_count = 0
 count = 0
 for i in date:
@@ -10,4 +13,6 @@ for i in date:
         if max_count < count:
             max_count = count
         count = 0
+if max_count < count:
+    max_count = count
 print(max_count)
