9dc014a8a681cc6daf20ac551c7e44836b066521
Dima <dimaszver@live.com>
2023-07-12 16:44:42 +0300

    Перенесли файлы первого семинара в папку

--- les1.2.py
+++
@@ -1,8 +0,0 @@
-## 3 Задача
-year = int(input ('Введите год - '))
-if (year%4 == 0) or (year%100 != 0) and (year%400 == 0):
-    print (f'{year} Год високосный')
-else:
-     print (f'{year} Год невисокосный')
-
-
